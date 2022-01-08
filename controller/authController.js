50b86d04b9978afd3b01c6743843ecaaa74b6beb
ssshivam1206 <ssshivam1206@gmail.com>
2022-01-09 00:36:21 +0530

    email complete

--- controller/authController.js
+++ controller/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -43,6 +43,10 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 });
@@ -117,13 +121,14 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     'host'
   )}/api/v1/users/resetPassword/${resetToken}`;
 
-  const message = `Forgot Password? Submit the path request to with your new password and passwordConfirm to : ${resetURL}.\n if you didn't forgot then go and login again `;
+  //const message = `Forgot Password? Submit the path request to with your new password and passwordConfirm to : ${resetURL}.\n if you didn't forgot then go and login again `;
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'your password reset token',
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'your password reset token',
+    //   message,
+    // });
+    await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
       status: 'success',
--- utils/email.js
+++ utils/email.js
@@ -1,25 +1,61 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  // 1) create a transpoter
-  const transpoter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  // 2) define the email options
-  const mailOptions = {
-    from: 'Shivam Singh sshivam1206@gmail.com',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Shivam Singh ${process.env.EMAIL_FROM}`;
+  }
 
-  // 3) send email
-  await transpoter.sendMail(mailOptions);
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      const client = nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+      return client;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // send actaul email
+  async send(template, subject) {
+    // 1) render hmtl based pug
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+    // 2) define email option
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      text: htmlToText.fromString(html),
+    };
+
+    // 3) create a transpoter
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the natours family !!');
+  }
+
+  async sendPasswordReset() {
+    await this.send('passwordReset', 'Your password Reset token is here ');
+  }
 };
-
-module.exports = sendEmail;
